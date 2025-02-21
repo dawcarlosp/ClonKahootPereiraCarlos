3ca0e04cce2288324f6f2cdd277b5f3bc23ccba2
dawcarlosp <dawcarlospereira@gmail.com>
2025-02-21 14:03:35 +0100

    Avance a la hora de leer el id de las preguntas

---
+++ modulos/preguntas/js/app.js
@@ -0,0 +1,43 @@
+let segundos = 10;
+let aux = 0;
+let h1 = document.getElementById("cuentaAtras");
+let dialog = document.getElementById("modal");
+let botones = document.querySelectorAll("button");
+let posiblesRespuestas = document.getElementById("posiblesRespuestas");
+let intervalo = setInterval(cuentaAtras, 1000);
+function cuentaAtras (){
+    h1.textContent = segundos - aux;
+    aux++;
+    if (aux == 12){
+        clearInterval(intervalo);
+        h1.textContent = "Fin";
+        dialog.style.display = "none";
+        dialog.close();
+    }
+}
+async function obtenerPreguntas() {
+    try {
+      const response = await fetch("js/preguntas.json");
+      if (!response.ok) {
+        throw new Error("Error al obtener las preguntas");
+      }
+      const preguntas = await response.json();
+      console.log(preguntas);
+      preguntas.forEach(pregunta => {
+        alert(pregunta.id)
+      })
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+  obtenerPreguntas();
+/*
+botones.forEach(boton => boton.addEventListener("click", function (){validarRespuestas(boton.value)}));
+function validarRespuestas(valor){
+    if(valor === "C"){
+        alert("Respuesta correcta");
+    }else{
+        alert("Respuesta incorrecta");
+    }
+}
+*/
